dd34606b80306573cb410c6034c38b9c6c80c46b
unknown <rrrr0ffff@gmail.com>
2023-07-20 14:37:53 -0300

    added basic header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+	return (
+		<div className="bg-orange-300">
+			<div className="container">
+				<div className="">⌨</div>
+				<nav>
+					<a href="">Products</a>
+					<a href="">About</a>
+					<a href="">Contact</a>
+				</nav>
+			</div>
+		</div>
+	);
+}
