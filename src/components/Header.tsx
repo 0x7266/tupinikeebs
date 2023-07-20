2ed8d297ec3dc8f1d493b2c3648e8f256b3263f0
unknown <rrrr0ffff@gmail.com>
2023-07-20 19:55:17 -0300

    fixed links alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ export function Header() {
 	return (
 		<div className="flex justify-around items-center">
 			<div className="text-6xl">⌨</div>
-			<nav className="flex items-center text-xl font-semibold gap-6">
+			<nav className="flex items-baseline text-xl font-semibold gap-6">
 				<Link to="/products">Products</Link>
 				<Link to="/about">About</Link>
 				<Link to="/faq">FAQ</Link>
