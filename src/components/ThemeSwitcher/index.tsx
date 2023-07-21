7d6ad6f2278cadd0dbf493513957d4e945dbd660
unknown <rrrr0ffff@gmail.com>
2023-07-21 00:56:32 -0300

    changed theme switcher button style

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -1,37 +1,49 @@
-import { MouseEvent, useState } from "react";
+import { MouseEvent, ReactElement, useState } from "react";
 import ComputerIcon from "./ComputerIcon";
 import MoonIcon from "./MoonIcon";
 import SunIcon from "./SunIcon";
 
+type Theme = {
+	name: string;
+	icon: ReactElement;
+};
+
 export function ThemeSwitcher() {
 	const [currentTheme, setCurrentTheme] = useState<string>("system");
 	const [isOpen, setIsOpen] = useState<boolean>(false);
-	const themes = [
+	const [themes, setThemes] = useState<Theme[]>([
 		{ name: "dark", icon: <MoonIcon /> },
 		{ name: "light", icon: <SunIcon /> },
 		{ name: "system", icon: <ComputerIcon /> },
-	];
+	]);
 
 	function handleClick(e: MouseEvent<HTMLButtonElement>) {
 		e.preventDefault();
+		const themeName = e.currentTarget.name;
 		setIsOpen((prev) => !prev);
-		setCurrentTheme(e.currentTarget.name);
+		setCurrentTheme(themeName);
+		setThemes((prev) => [
+			themes.find((theme) => theme.name === themeName)!,
+			...prev.filter((theme) => theme.name !== themeName),
+		]);
 	}
 
 	return (
-		<div className="absolute flex border border-gray-950 rounded-full px-1 py-2">
-			{themes.map((theme, index) => (
-				<button
-					key={index}
-					onClick={handleClick}
-					name={theme.name}
-					className={`${
-						isOpen ? "" : currentTheme === theme.name ? "" : "hidden"
-					}`}
-				>
-					{theme.icon}
-				</button>
-			))}
+		<div className="relative">
+			<div className="absolute flex flex-col border border-gray-950 rounded-full px-2 py-2">
+				{themes.map((theme, index) => (
+					<button
+						key={index}
+						onClick={handleClick}
+						name={theme.name}
+						className={`${
+							isOpen ? "" : currentTheme === theme.name ? "" : "hidden"
+						}`}
+					>
+						{theme.icon}
+					</button>
+				))}
+			</div>
 		</div>
 	);
 }
