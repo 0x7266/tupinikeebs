521c2771cce7302dc38391282dcf43a000e7e985
unknown <rrrr0ffff@gmail.com>
2023-08-05 19:14:52 -0300

    changed logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { ThemeSwitcher } from "./ThemeSwitcher";
+import logo from "../assets/logo.png";
+
 export function Header() {
 	const links = [
 		{ path: "/products", name: "Products" },
@@ -8,11 +10,13 @@ export function Header() {
 	];
 
 	return (
-		<div className="flex justify-center bg-secondary dark:bg-accent dark:text-zinc-950 w-full py-5">
+		<div className="flex justify-center dark:text-text-dark w-full">
 			<div className="container flex flex-col gap-3 md:flex-row justify-around items-center">
-				<div className="flex flex-col md:flex-row items-center">
-					<span className="text-9xl md:text-6xl">⌨</span>
-					<span className="text-5xl md:text-2xl font-bold">TupiniKeebs</span>
+				<div className="flex flex-col md:flex-row gap-2 items-center relative">
+					<img className="w-28" src={logo} />
+					<span className="text-5xl md:text-2xl font-bold relative -top-6 md:-top-0">
+						TupiniKeebs
+					</span>
 				</div>
 				<nav className="flex items-center text-xl font-semibold gap-6 w-full md:w-fit justify-around md:justify-center">
 					{links.map((link, index) => (
