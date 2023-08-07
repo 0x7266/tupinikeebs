0871fa8f5cf27d7d675574e6d2f7a106daf55957
unknown <rrrr0ffff@gmail.com>
2023-08-06 23:12:49 -0300

    fixed height

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import Product from "./components/Product";
 function App() {
 	return (
 		<BrowserRouter>
-			<div className="h-[100dvh] bg-background text-text dark:bg-background-dark dark:text-text-dark flex flex-col items-center font-mono gap-5">
-				<div className="container h-full py-5 flex flex-col items-center gap-20">
+			<div className="min-h-[100dvh] bg-background text-text dark:bg-background-dark dark:text-text-dark flex flex-col items-center font-mono gap-5 overflow-hidden">
+				<div className="container h-full py-5 flex flex-col items-center gap-12 md:gap-20">
 					<Header />
 					<Routes>
 						<Route path="/" element={<Home />} />
