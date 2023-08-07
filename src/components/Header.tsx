7c04df8c1ad59e8c7b370d2777cf3353d229d8b4
unknown <rrrr0ffff@gmail.com>
2023-08-07 00:17:43 -0300

    added link to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,12 +12,14 @@ export function Header() {
 	return (
 		<header className="flex justify-center dark:text-text-dark w-full">
 			<div className="container flex flex-col gap-3 md:flex-row justify-around items-center">
-				<div className="flex flex-col md:flex-row gap-4 items-center relative">
-					<img className="w-28" src={logo} />
-					<span className="text-5xl md:text-2xl font-bold relative -top-4 md:-top-0">
-						TupiniKeebs
-					</span>
-				</div>
+				<Link to="/">
+					<div className="flex flex-col md:flex-row gap-4 items-center relative">
+						<img className="w-28" src={logo} />
+						<span className="text-5xl md:text-2xl font-bold relative -top-4 md:-top-0">
+							TupiniKeebs
+						</span>
+					</div>
+				</Link>
 				<nav className="flex items-center text-xl font-semibold md:gap-6 w-full md:w-fit justify-around md:justify-center">
 					{links.map((link, index) => (
 						<Link to={link.path} key={index}>
