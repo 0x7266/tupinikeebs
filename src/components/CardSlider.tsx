6ab9fb391e28a79d8ced165388d7d97eaf951f4e
unknown <rrrr0ffff@gmail.com>
2023-08-06 22:47:07 -0300

    changed initial position os cards on stack

--- src/components/CardSlider.tsx
+++ src/components/CardSlider.tsx
@@ -5,9 +5,21 @@ import img2 from "../assets/20230318_121927-2-scaled-3-1024x512.jpg";
 import img3 from "../assets/20230318_122127-scaled-2-1024x512.jpg";
 
 const cards = [
-	{ img: img1, position: { x: -170, y: -35, rotate: -25 } },
-	{ img: img2, position: { x: 160, y: -25, rotate: 35 } },
-	{ img: img3, position: { x: 0, y: -20 } },
+	{
+		img: img1,
+		initialPosition: { x: -2, y: 3, rotate: 4 },
+		finalPosition: { x: -170, y: -35, rotate: -25 },
+	},
+	{
+		img: img2,
+		initialPosition: { x: 4, y: -2, rotate: -5 },
+		finalPosition: { x: 160, y: -25, rotate: 35 },
+	},
+	{
+		img: img3,
+		initialPosition: { x: 0, y: 0, rotate: 0 },
+		finalPosition: { x: 0, y: -20, rotate: 0 },
+	},
 ];
 
 export default function CardSlider() {
@@ -26,9 +38,11 @@ export default function CardSlider() {
 						key={index}
 						className={`absolute h-[400px] w-[300px] object-cover rounded-xl transition-all duration-700 border-8 border-black`}
 						style={{
-							x: hovered ? card.position.x : 0,
-							y: hovered ? card.position.y : 0,
-							rotate: hovered ? card.position.rotate : 0,
+							x: hovered ? card.finalPosition.x : card.initialPosition.x,
+							y: hovered ? card.finalPosition.y : card.initialPosition.y,
+							rotate: hovered
+								? card.finalPosition.rotate
+								: card.initialPosition.rotate,
 						}}
 					/>
 				))}
