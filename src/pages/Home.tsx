483ab6be312096e60de10c73eb76a0b33a2d544d
unknown <rrrr0ffff@gmail.com>
2023-08-07 14:56:24 -0300

    rem oved dead code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,23 +11,6 @@ export function Home() {
 	return (
 		<div className="flex flex-col items-center">
 			<section className="grid md:grid-cols-2 h-full md:w-4/5 px-5">
-				{/* <div
-				className="left w-96 h-96"
-				style={{
-					backgroundImage: `url(${corneLeft})`,
-					backgroundSize: "100%",
-					backgroundRepeat: "no-repeat",
-					opacity: 0.2,
-				}}
-				></div>
-				<div
-				className="right w-96 h-96"
-				style={{
-					backgroundImage: `url(${corneRight})`,
-					backgroundSize: "100%",
-					backgroundRepeat: "no-repeat",
-				}}
-			></div> */}
 				{/* <video src={video} autoPlay muted loop></video> */}
 				<div className="flex flex-col gap-2 md:gap-5 xl:w-4/5 items-center md:items-start justify-self-end">
 					<h1 className="text-5xl md:text-6xl font-bold">BLABLABLA</h1>
