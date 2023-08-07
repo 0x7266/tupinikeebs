3f405d2763a2eed6eda176a922605cc10c9b4013
unknown <rrrr0ffff@gmail.com>
2023-08-06 23:34:19 -0300

    adadded contact links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 // import corneLeft from "../assets/corne_left.png";
 // import corneRight from "../assets/corne_right.png";
 // import video from "../assets/home_video.webm";
-import CardStack from "../components/CardStack";
+import { Link } from "react-router-dom";
+import typingImg from "../assets/typing.webp";
+import emailIcon from "../assets/email_icon.webp";
+import twitterIcon from "../assets/twitter_icon.png";
 
 export function Home() {
 	return (
-		<div className="grid md:grid-cols-2 h-full md:w-4/5 gap-10">
+		<div className="grid md:grid-cols-2 h-full md:w-4/5 px-5">
 			{/* <div
 				className="left w-96 h-96"
 				style={{
@@ -24,16 +27,18 @@ export function Home() {
 				}}
 			></div> */}
 			{/* <video src={video} autoPlay muted loop></video> */}
-			<div className="flex flex-col gap-5 items-center w-4/5 mx-auto md:w-2/3">
-				<h1 className="text-6xl font-bold">LOREM IPSUM</h1>
-				<p className="text-lg">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-					deserunt veniam id alias quas eligendi sint. Natus omnis tempora
-					magnam autem recusandae, assumenda id ex distinctio? Eaque sit
-					excepturi reiciendis.
-				</p>
+			<div className="flex flex-col gap-1 md:gap-5 xl:w-4/5 items-center md:items-start justify-self-end">
+				<h1 className="text-5xl md:text-6xl font-bold">PEÇA JÁ O SEU</h1>
+				<div className="flex gap-5">
+					<Link to="">
+						<img src={twitterIcon} alt="twitter" className="w-16" />
+					</Link>
+					<Link to="">
+						<img src={emailIcon} alt="email me" className="w-16" />
+					</Link>
+				</div>
 			</div>
-			<CardStack />
+			<img src={typingImg} alt="" />
 		</div>
 	);
 }
