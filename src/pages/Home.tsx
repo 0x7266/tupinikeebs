3a17cc608d583218e6122ec0fc0907802e950c08
unknown <rrrr0ffff@gmail.com>
2023-08-09 00:56:05 -0300

    changed layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,31 +6,32 @@ import twitterIcon from "../assets/icons/twitter_icon.png";
 
 export function Home() {
 	return (
-		<div className="flex flex-col items-center">
-			<section className="grid md:grid-cols-2 h-full md:w-4/5 px-5">
-				{/* <video src={video} autoPlay muted loop></video> */}
-				<div className="flex flex-col gap-2 md:gap-5 xl:w-4/5 items-center md:items-start justify-self-end">
-					<h1 className="text-5xl md:text-6xl font-bold">BLABLABLA</h1>
-					<div className="">
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
-						commodi, quisquam, odio hic, perspiciatis ut recusandae neque quod
-						dolor inventore nostrum rerum quasi laboriosam itaque soluta? Ad
-						culpa ut temporibus.
-					</div>
-					<div className="flex gap-5">
-						<Link to="https://twitter.com/neversaytop">
-							<img src={twitterIcon} alt="twitter" className="w-16" />
-						</Link>
-						<Link to="">
-							<img src={telegramIcon} alt="telegram" className="w-16" />
-						</Link>
-						<Link to="">
-							<img src={emailIcon} alt="email me" className="w-16" />
-						</Link>
-					</div>
+		<section className="h-[300dvh] px-5 flex flex-col items-center">
+			<div className="flex flex-col gap-2 md:gap-5 w-2/5 items-center justify-self-end fixed">
+				<h1 className="text-5xl md:text-6xl font-bold">PEÇA JÁ O SEU</h1>
+				<div className="">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+					commodi, quisquam, odio hic, perspiciatis ut recusandae neque quod
+					dolor inventore nostrum rerum quasi laboriosam itaque soluta? Ad culpa
+					ut temporibus.
 				</div>
-				<img src={typingImg} alt="" className="place-self-center" />
-			</section>
-		</div>
+				<div className="flex items-center justify-center gap-5">
+					<Link to="https://twitter.com/neversaytop">
+						<img src={twitterIcon} alt="twitter" className="w-16" />
+					</Link>
+					<Link to="">
+						<img src={telegramIcon} alt="telegram" className="w-12" />
+					</Link>
+					<Link to="">
+						<img src={emailIcon} alt="email me" className="w-16" />
+					</Link>
+				</div>
+			</div>
+			<img
+				src={typingImg}
+				alt=""
+				className="place-self-center relative top-[60vh]"
+			/>
+		</section>
 	);
 }
