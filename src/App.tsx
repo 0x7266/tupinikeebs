a4c34b3243004ab19f3c75b8d7aad7ceebace6a3
unknown <rrrr0ffff@gmail.com>
2023-08-11 11:10:28 -0300

    added glow on hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 import { Header } from "./components";
 import { Home, Products } from "./pages";
 import Product from "./components/Product";
 
 function App() {
+	const mouseX = useMotionValue(0);
+	const mouseY = useMotionValue(0);
+
+	function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
+		console.log(clientX);
+		const { left, top } = currentTarget.getBoundingClientRect();
+		mouseX.set(clientX - left);
+		mouseY.set(clientY - top);
+	}
+
 	return (
 		<BrowserRouter>
-			<div className="min-h-[100dvh] text-text-dark flex flex-col items-center font-mono gap-5 overflow-x-clip">
-				<div className="container h-full flex flex-col items-center gap-12 md:gap-20">
-					<Header />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/products" element={<Products />} />
-						<Route path="/:id" element={<Product />} />
-					</Routes>
+			<motion.div
+				onMouseMove={handleMouseMove}
+				className="min-h-[100dvh] bg text-text-dark  font-mono gap-5 overflow-x-clip relative"
+			>
+				<div className="w-full h-full backdrop-brightness-[25%] flex flex-col items-center group">
+					<motion.div
+						className="pointer-events-none absolute -inset-px opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-3xl"
+						style={{
+							background: useMotionTemplate`radial-gradient(300px circle at ${mouseX}px ${mouseY}px, rgba(200, 200, 200, .15), transparent 80%)`,
+						}}
+					/>
+					<div className="container h-full flex flex-col items-center gap-12 md:gap-20">
+						<Header />
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/products" element={<Products />} />
+							<Route path="/:id" element={<Product />} />
+						</Routes>
+					</div>
 				</div>
-			</div>
+			</motion.div>
 		</BrowserRouter>
 	);
 }
