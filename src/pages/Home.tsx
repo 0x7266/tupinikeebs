386314e79e753cfaf1b633d3c05eddfe3c022b1f
unknown <rrrr0ffff@gmail.com>
2023-08-11 14:38:11 -0300

    fixed path to image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,60 +1,109 @@
-import { Link } from "react-router-dom";
-import { motion, useScroll, useTransform } from "framer-motion";
-import typingImg from "../assets/typing.webp";
-import emailIcon from "../assets/icons/email_icon.webp";
-import telegramIcon from "../assets/icons/telegram_icon.png";
-import twitterIcon from "../assets/icons/twitter_icon.png";
+import { useTransform, useScroll, motion } from "framer-motion";
 import { useRef } from "react";
 
-export function Home() {
-	const containerRef = useRef(null);
+export const Home = () => {
+	const targetRef = useRef<HTMLDivElement | null>(null);
+	const extendedRef = useRef<HTMLDivElement | null>(null);
 	const { scrollYProgress } = useScroll({
-		// offset: ["start end", "end end"],
+		target: targetRef,
+		offset: ["start end", "end end"],
 	});
-	const scale = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
-	const opacity = useTransform(scrollYProgress, [0, 0.2], [0, 1]);
-	const translate = useTransform(scrollYProgress, [0.6, 0.8], ["0px", "-30vw"]);
+	const { scrollYProgress: scrollYProgressIncludingOverlap } = useScroll({
+		target: extendedRef,
+		offset: ["start end", "end end"],
+	});
+
+	const scale = useTransform(
+		scrollYProgressIncludingOverlap,
+		[0.1, 0.4, 0.75, 1],
+		[1, 2.5, 4.2, 1]
+	);
+	const x = useTransform(
+		scrollYProgressIncludingOverlap,
+		[0.1, 0.25, 0.75, 1],
+		["0vw", "-55vw", "-135vw", "-18vw"]
+	);
+	const y = useTransform(
+		scrollYProgressIncludingOverlap,
+		[0.75, 1],
+		["0vh", "40vh"]
+	);
+	const opacity = useTransform(scrollYProgress, [0.9, 1], [1, 0]);
+
+	const avatarGroupOpacity = useTransform(
+		scrollYProgress,
+		[0, 0.23, 0.25],
+		[0, 0, 1]
+	);
+
+	const avatarGroupX = useTransform(
+		scrollYProgress,
+		[0, 0.23, 0.25, 0.4, 0.45, 0.6, 0.65],
+		["60px", "60px", "40px", "40px", "20px", "20px", "0px"]
+	);
+
+	const avatarOneScale = useTransform(
+		scrollYProgress,
+		[0, 0.23, 0.25, 0.85, 0.9],
+		[0, 0, 1, 1, 0]
+	);
+
+	const avatarTwoScale = useTransform(
+		scrollYProgress,
+		[0, 0.4, 0.45],
+		[0, 0, 1]
+	);
+
+	const avatarTwoOpacity = useTransform(
+		scrollYProgressIncludingOverlap,
+		[0.9999, 1],
+		[1, 0]
+	);
+
+	const avatarThreeScale = useTransform(
+		scrollYProgress,
+		[0, 0.6, 0.65, 0.85, 0.9],
+		[0, 0, 1, 1, 0]
+	);
 
 	return (
-		<motion.section
-			ref={containerRef}
-			className="flex flex-col items-center mt-[-75vh]"
+		<section
+			ref={targetRef}
+			className="relative z-10 mt-[-30vh] h-[300vh] border"
 		>
-			<motion.div
-				style={{ scale, opacity, translateX: translate }}
-				className="h-[90dvh] flex flex-col gap-2 md:gap-5 w-2/5 items-center"
-			>
-				<div className="group relative">
-					<div className="absolute blur bg-accent transition duration-200 rounded-md -inset-1 opacity-80 animate-tilt"></div>
-					<h1 className="text-5xl md:text-6xl font-bold relative bg-background-dark px-5 py-2 rounded-md group-hover:text-secondary transition duration-300">
-						PEÇA JÁ O SEU
-					</h1>
+			<div ref={extendedRef} className="mb-[-120vh] h-[420vh] w-full">
+				<div className="sticky top-[10vh]">
+					<div className="flex justify-center">
+						<motion.div style={{ scale, x, y }} className="origin-top">
+							<motion.img
+								style={{ opacity }}
+								src="/src/assets/logo.png"
+								className="max-h-none w-[70vw] h-[50vw] bg-orange-300"
+							/>
+							<motion.div
+								style={{ opacity: avatarGroupOpacity, x: avatarGroupX }}
+								className="absolute right-[10%] top-[1.5%] flex gap-2"
+							>
+								<motion.img
+									style={{ scale: avatarOneScale }}
+									className="h-[1.5vw] w-[1.5vw] rounded-full border border-[#4ca] object-cover"
+									src="https://unsplash.com/photos/_H6wpor9mjs/download?force=true&w=128&h=128"
+								/>
+								<motion.img
+									style={{ scale: avatarTwoScale, opacity: avatarTwoOpacity }}
+									className="h-[1.5vw] w-[1.5vw] rounded-full border border-[#c82] object-cover"
+									src="https://unsplash.com/photos/sibVwORYqs0/download?force=true&w=128&h=128"
+								/>
+								<motion.img
+									style={{ scale: avatarThreeScale }}
+									className="h-[1.5vw] w-[1.5vw] rounded-full border border-[#f0f] object-cover"
+									src="https://unsplash.com/photos/7YVZYZeITc8/download?force=true&w=128&h=128"
+								/>
+							</motion.div>
+						</motion.div>
+					</div>
 				</div>
-				<div className="w-2/3">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
-					commodi, quisquam, odio hic, perspiciatis ut recusandae neque quod
-					dolor inventore nostrum rerum quasi laboriosam itaque soluta?
-				</div>
-				<div className="flex items-center justify-center gap-5">
-					<Link to="https://twitter.com/neversaytop">
-						<img src={twitterIcon} alt="twitter" className="w-16" />
-					</Link>
-					<Link to="">
-						<img src={telegramIcon} alt="telegram" className="w-12" />
-					</Link>
-					<Link to="">
-						<img src={emailIcon} alt="email me" className="w-16" />
-					</Link>
-				</div>
-				<div>
-					<motion.img
-						// style={{ scale: imgScale }}
-						src={typingImg}
-						alt=""
-						className="place-self-center relative"
-					/>
-				</div>
-			</motion.div>
-		</motion.section>
+			</div>
+		</section>
 	);
-}
+};
