916ea9ed6b42b594a4922cedd200079f5bf4d95d
unknown <rrrr0ffff@gmail.com>
2023-08-10 23:58:41 -0300

    changed animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,31 +9,31 @@ import { useRef } from "react";
 export function Home() {
 	const containerRef = useRef(null);
 	const { scrollYProgress } = useScroll({
-		// target: containerRef,
+		// offset: ["start end", "end end"],
 	});
-	const imgScale = useTransform(scrollYProgress, [0, 0.2], [0.3, 1]);
-	const opacity = useTransform(scrollYProgress, [0, 0.3], [0, 1]);
+	const scale = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
+	const opacity = useTransform(scrollYProgress, [0, 0.2], [0, 1]);
+	const translate = useTransform(scrollYProgress, [0.8, 1], ["0px", "-30vw"]);
 
 	return (
 		<motion.section
 			ref={containerRef}
-			className="flex flex-col items-center relative mt-[-70vh]"
+			className="flex flex-col items-center mt-[-65vh] sticky top-[20vh]"
 		>
 			<motion.div
-				style={{ opacity }}
-				className="h-[200dvh] flex flex-col gap-2 md:gap-5 w-2/5 items-center justify-self-end"
+				style={{ scale, opacity, translateX: translate }}
+				className="h-[90dvh] flex flex-col gap-2 md:gap-5 w-2/5 items-center"
 			>
-				<div className="group sticky top-0">
+				<div className="group relative">
 					<div className="absolute blur bg-accent transition duration-200 rounded-md -inset-1 opacity-80 animate-tilt"></div>
 					<h1 className="text-5xl md:text-6xl font-bold relative bg-background-dark px-5 py-2 rounded-md text-text-dark group-hover:text-secondary transition duration-500">
 						PEÇA JÁ O SEU
 					</h1>
 				</div>
-				<div className="">
+				<div className="w-2/3">
 					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
 					commodi, quisquam, odio hic, perspiciatis ut recusandae neque quod
-					dolor inventore nostrum rerum quasi laboriosam itaque soluta? Ad culpa
-					ut temporibus.
+					dolor inventore nostrum rerum quasi laboriosam itaque soluta?
 				</div>
 				<div className="flex items-center justify-center gap-5">
 					<Link to="https://twitter.com/neversaytop">
@@ -48,7 +48,7 @@ export function Home() {
 				</div>
 				<div>
 					<motion.img
-						style={{ scale: imgScale }}
+						// style={{ scale: imgScale }}
 						src={typingImg}
 						alt=""
 						className="place-self-center relative"
