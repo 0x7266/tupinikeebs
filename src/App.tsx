7e51bb9d8b6ac89667218420e803d04bdd97c2e8
unknown <rrrr0ffff@gmail.com>
2023-08-12 19:22:35 -0300

    moved gradient to header

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,10 @@ import Slider from "./components/Slider";
 import Carousel from "./components/Carousel";
 
 function App() {
-	const mouseX = useMotionValue(0);
-	const mouseY = useMotionValue(0);
-
-	function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
-		if (currentTarget) {
-			const { left, top } = currentTarget.getBoundingClientRect();
-			mouseX.set(clientX - left);
-			mouseY.set(clientY - top);
-		}
-	}
-
 	return (
 		<BrowserRouter>
-			<motion.div
-				onMouseMove={handleMouseMove}
-				className="text-text-dark font-mono overflow-x-clip"
-			>
+			<div className="text-text-dark font-mono overflow-x-clip">
 				<div className="h-full backdrop-brightness-[25%] flex flex-col items-center group">
-					<motion.div
-						className="pointer-events-none absolute -inset-px opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-3xl"
-						style={{
-							background: useMotionTemplate`radial-gradient(300px circle at ${mouseX}px ${mouseY}px, rgba(140, 140, 140, .20), transparent 80%)`,
-						}}
-					/>
 					<Header />
 					<Carousel />
 					{/* <div className="container flex flex-col items-center gap-12 md:gap-20">
@@ -41,7 +21,7 @@ function App() {
 						</Routes>
 					</div> */}
 				</div>
-			</motion.div>
+			</div>
 		</BrowserRouter>
 	);
 }
