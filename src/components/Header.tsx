9dadae2933d425db1301b562af009313ad580fb2
unknown <rrrr0ffff@gmail.com>
2023-08-12 19:22:27 -0300

    changed gradient

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import { motion, useScroll, useTransform } from "framer-motion";
+import {
+	motion,
+	useMotionTemplate,
+	useMotionValue,
+	useScroll,
+	useTransform,
+} from "framer-motion";
 import { useRef } from "react";
 
 export function Header() {
@@ -21,14 +27,31 @@ export function Header() {
 	const position = useTransform(scrollYProgress, (pos) =>
 		pos >= 1 ? "relative" : "fixed"
 	);
-	const scale = useTransform(scrollYProgress, [0, 0.3], [1, 0.6]);
+	const scale = useTransform(scrollYProgress, [0, 0.3], [1, 0.4]);
+
+	const mouseX = useMotionValue(0);
+	const mouseY = useMotionValue(0);
+
+	function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
+		if (currentTarget) {
+			const { left, top } = currentTarget.getBoundingClientRect();
+			mouseX.set(clientX - left);
+			mouseY.set(clientY - top);
+		}
+	}
 
 	return (
-		<header className="bg w-full h-screen">
+		<motion.header onMouseMove={handleMouseMove} className="bg w-full h-screen">
+			<motion.div
+				className="pointer-events-none absolute -inset-px opacity-0 group-hover:opacity-100 transition-all duration-300 rounded-3xl"
+				style={{
+					background: useMotionTemplate`radial-gradient(circle farthest-side at ${mouseX}px ${mouseY}px, rgba(170, 170, 170, .3), transparent 100%)`,
+				}}
+			/>
 			<div className="backdrop-brightness-[25%] h-full">
 				<motion.div
 					ref={ref}
-					className="w-full h-full flex justify-center pt-10"
+					className="w-full h-full flex justify-center pt-20"
 				>
 					<motion.div
 						style={{ position, scale, opacity }}
@@ -52,6 +75,6 @@ export function Header() {
 					</motion.div>
 				</motion.div>
 			</div>
-		</header>
+		</motion.header>
 	);
 }
