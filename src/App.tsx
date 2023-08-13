1b31bea1e41cd197887c3d69e264869b01fa2a17
unknown <rrrr0ffff@gmail.com>
2023-08-13 15:15:32 -0300

    added animatepresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence } from "framer-motion";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components";
 import { Home, Products } from "./pages";
@@ -5,20 +6,22 @@ import Product from "./components/Product";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<div className="bg min-h-screen">
-				<div className="min-h-[100dvh] bg-opacity-70 dark:bg-opacity-95 bg-background text-text dark:bg-background-dark dark:text-text-dark flex flex-col items-center font-mono gap-5 overflow-hidden backdrop-blur-sm">
-					<div className="container h-full py-5 flex flex-col items-center gap-12 md:gap-20">
-						<Header />
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/products" element={<Products />} />
-							<Route path="/:id" element={<Product />} />
-						</Routes>
+		<AnimatePresence>
+			<BrowserRouter>
+				<div className="bg min-h-screen">
+					<div className="min-h-[100dvh] bg-opacity-70 dark:bg-opacity-95 bg-background text-text dark:bg-background-dark dark:text-text-dark flex flex-col items-center font-mono gap-5 overflow-hidden backdrop-blur-sm">
+						<div className="container h-full py-5 flex flex-col items-center gap-12 md:gap-20">
+							<Header />
+							<Routes>
+								<Route path="/" element={<Home />} />
+								<Route path="/products" element={<Products />} />
+								<Route path="/:id" element={<Product />} />
+							</Routes>
+						</div>
 					</div>
 				</div>
-			</div>
-		</BrowserRouter>
+			</BrowserRouter>
+		</AnimatePresence>
 	);
 }
 
