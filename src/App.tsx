49e1eeb91fb50017674b4c603dd550d710cbd982
unknown <rrrr0ffff@gmail.com>
2023-08-15 00:28:58 -0300

    removed dead code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { AnimatePresence } from "framer-motion";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components";
-import { Home, Products } from "./pages";
-import Product from "./components/Product";
 import Carousel from "./components/Carousel";
 
 function App() {
@@ -13,12 +11,7 @@ function App() {
 					<div className="backdrop-brightness-[20%] shadow-small md:shadow-large">
 						<div className="container mx-auto py-5 flex flex-col gap-20 items-center font-mono text-text-dark min-h-screen">
 							<Header />
-							<Carousel></Carousel>
-							<Routes>
-								<Route path="/" element={<Home />} />
-								<Route path="/products" element={<Products />} />
-								<Route path="/:id" element={<Product />} />
-							</Routes>
+							<Carousel />
 						</div>
 					</div>
 				</div>
