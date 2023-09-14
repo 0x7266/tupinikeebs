fa5966a5d105bec75268694fad74bec62fe9e608
unknown <rrrr0ffff@gmail.com>
2023-09-14 19:08:34 -0300

    changed shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
 				one: ["Intel One Mono", "monospace"],
 			},
 			boxShadow: {
-				large: "inset 0 0 15vh 15vh #000000dd",
+				large: "inset 0 0 18vh 20vh #000000dd",
 				small: "inset 0 0 15rem 2vw #000000dd",
 			},
 		},
